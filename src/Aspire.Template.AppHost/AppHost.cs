3e8e5586adec142e5e59dea76dcadb9672a9a4eb
Brian Davis <kbdavis07@yahoo.com>
2026-03-13 17:15:34 -0400

    Added postgres

--- src/Aspire.Template.AppHost/AppHost.cs
+++ src/Aspire.Template.AppHost/AppHost.cs
@@ -1,14 +1,28 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder.AddPostgres("postgres")
+    .WithPgAdmin()
+    .WithLifetime(ContainerLifetime.Persistent);
+
+if (builder.ExecutionContext.IsRunMode)
+{
+    // Data volumes don't work on ACA for Postgres so only add when running
+    postgres.WithDataVolume();
+}
+
+var templateDb = postgres.AddDatabase("templatedb");
+
 var cache = builder.AddRedis("cache");
 
-var server = builder.AddProject<Projects.Aspire_Template_Server>("server")
+var server = builder.AddProject<Projects.Aspire_Template_Server>("apiserver")
+    .WithReference(templateDb)
+    .WaitFor(templateDb)
     .WithReference(cache)
     .WaitFor(cache)
     .WithHttpHealthCheck("/health")
     .WithExternalHttpEndpoints();
 
-var webfrontend = builder.AddViteApp("webfrontend", "../frontend")
+var webfrontend = builder.AddViteApp("WebUI", "../frontend")
     .WithReference(server)
     .WaitFor(server);
 
